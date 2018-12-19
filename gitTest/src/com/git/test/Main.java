ad63f47e5b00676174fef7dc0edef668d59f5ca0
user1 <user1@user1-PC>
2018-12-19 13:23:35 +0900

    불러오기

--- gitTest/src/com/git/test/Main.java
+++ gitTest/src/com/git/test/Main.java
@@ -3,7 +3,9 @@ package com.git.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("git Ã³À½! 1¹ø");
+		System.out.println("firstUpdate");
+		System.out.println("secondUpdate");
+
 	}
 
 }
