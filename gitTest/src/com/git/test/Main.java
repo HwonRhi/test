f72350d73334ba7acab90b0c6b4dcd3d3b10b4eb
user1 <user1@user1-PC>
2018-12-19 13:14:40 +0900

    첫번째입니다.

--- gitTest/src/com/git/test/Main.java
+++ gitTest/src/com/git/test/Main.java
@@ -3,9 +3,7 @@ package com.git.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("firstUpdate");
-		System.out.println("추추가");
-
+		System.out.println("git 처음! 1번");
 	}
 
 }
